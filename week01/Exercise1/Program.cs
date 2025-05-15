c26f5bbca106abae44327fb76d54693460e190de
Chris <cruizborja@byupathway.edu>
2025-05-14 23:21:13 -0600

    First homework

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        String last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}")
     }
 }
