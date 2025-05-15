c3d40727d982ea6052e37ff96c27390b98c4f379
Chris <cruizborja@byupathway.edu>
2025-05-14 23:28:13 -0600

    corrections on first assigment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,6 +11,6 @@ class Program
         Console.WriteLine("What is your last name? ");
         String last_name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}")
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
