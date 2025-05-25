5575adcc4e0d2c398f3ad2d4d79a612125307667
Chris <cruizborja@byupathway.edu>
2025-05-24 22:02:58 -0600

    Fourth exercise done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userAdding = Console.ReadLine();
+            userNumber = int.Parse(userAdding);
+
+            // adding numbers
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Part 1: Computing the sum, or total, of the numbers in the list.
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the maximum number
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
