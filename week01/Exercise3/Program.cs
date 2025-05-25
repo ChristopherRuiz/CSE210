dd79883c672618479c77fee68aa586029490fb4f
Chris <cruizborja@byupathway.edu>
2025-05-24 21:35:26 -0600

    Third exercise done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
+//For the stretch I made the program to choose the magic number
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        //Asking for the number
+        Console.WriteLine("I have a magic number in mind. Can you guess it?");
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out guess))
+            {
+                if (number > guess)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (number < guess)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+            }
+        }
+
     }
 }
